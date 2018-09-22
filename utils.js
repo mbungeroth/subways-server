4ce14f0311c8b1d40f8418f6728286b6640db711
mbungeroth <mbungeroth@gmail.com>
2018-09-22 19:48:45 -0400

    fix untilArrival func to account for differences in current time and last time mta updated data

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const convertTime = epoch => new Date(epoch * 1000).toLocaleTimeString();
 const currentEpochTime = () => Math.floor(Date.now() / 1000);
 const untilArrival = arrivalTime => {
   const minutesUntil = Math.floor((arrivalTime - currentEpochTime()) / 60);
-  if (minutesUntil === 0) {
+  if (minutesUntil <= 0) {
     return "Arriving Now"
   } else if (minutesUntil === 1) {
     return "1 Minute";
